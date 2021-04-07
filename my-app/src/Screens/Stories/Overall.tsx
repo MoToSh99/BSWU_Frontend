da245f442ba1b406ee028f9de5edaf0209b5bb59
Maxwell11dk <dallasmaxwell@outlook.com>
2021-04-07 16:01:53 +0200

    added alert for navigation

--- my-app/src/Screens/Stories/Overall.tsx
+++ my-app/src/Screens/Stories/Overall.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { makeStyles, Theme } from '@material-ui/core/styles';
-import { Typography, Box, Modal } from '@material-ui/core';
+import { Typography, Box, Modal, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from '@material-ui/core';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { UserDetail} from '../../Models';
 import CelebrityScores from '../../Components/CelebrityScores';
@@ -10,6 +10,8 @@ import Overlay from '../../Components/Overlay';
 import Tada from 'react-reveal/Tada';
 import { getGaugeColor } from '../../Helpers';
 import ModalCeleb from '../../Components/ModalCeleb';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 
 const useStyles = makeStyles<Theme, any>((theme) => ({
@@ -38,7 +40,20 @@ const useStyles = makeStyles<Theme, any>((theme) => ({
     abs1: {
       zIndex: 1,
       position: "relative",
+    },
+    arrowContainer: {
+      display: "flex",
+      width: "100%",
+      justifyContent: "space-between",
+      alignItems: "center",
+      height: 100
+    },
+    dialog:{
+    "& .MuiDialog-paper" : {
+      opacity: 0.9,
+      color: "red",
     }
+  }
 }));
     
 export interface OverallProps {
@@ -49,6 +64,7 @@ const Overall: FC<OverallProps> = ({user}) => {
     const classes = useStyles({});
     const [progress, setProgress] = React.useState(0.0);
     const [fade, setFade] = React.useState(false);
+    const [navigateHelp, setShowNavigateHelp] = React.useState(true);
 
     const gaugeColor = getGaugeColor(user.overallscore);
 
@@ -110,6 +126,24 @@ const Overall: FC<OverallProps> = ({user}) => {
           </Typography>
         </div>
         </FadeIn>
+        <Dialog className={classes.dialog} open={navigateHelp} onClose={() => {setShowNavigateHelp(false)}}>
+          <DialogContent className={classes.dialog}>
+            <DialogContentText>
+              Navigate the site by clicking on the left or right side of the screen
+            </DialogContentText>
+            <div className={classes.arrowContainer}>
+              <div style={{padding: 20}}>
+              <ArrowBackIcon fontSize="large"/>
+              </div>
+              <div style={{padding: 20}}>
+              <ArrowForwardIcon fontSize="large"/>
+              </div>
+            </div>
+            <DialogActions>
+              <Button onClick={() => {setShowNavigateHelp(false)}}>Close</Button>
+            </DialogActions>
+          </DialogContent>
+        </Dialog>
       </div>
       )
 }
