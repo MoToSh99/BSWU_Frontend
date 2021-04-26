79fb6a3fbb7d6070166e3056d280b8b36725ba9f
Toheed Sharif <info@toheed.dk>
2021-04-26 15:47:42 +0200

    slideshow egen

--- my-app/src/Screens/Loading.tsx
+++ my-app/src/Screens/Loading.tsx
@@ -1,16 +1,5 @@
 import React, { FC, useEffect, useState } from "react";
 import { makeStyles, Theme } from "@material-ui/core/styles";
-import Lottie from "react-lottie";
-import * as twitterbird from "../Images/Twitter.json";
-import * as friends from "../Images/friends.json";
-import * as verified from "../Images/verified.json";
-import * as geolocalization from "../Images/geolocalization.json";
-import * as development from "../Images/web-development.json";
-import * as legoData from "../Images/legoloading.json";
-import * as doneData from "../Images/doneloading.json";
-import * as analyze from "../Images/analyze.json";
-import * as press from "../Images/press.json";
-import * as abraham from "../Images/abraham.json";
 import FadeIn from "react-fade-in";
 import {
   Typography,
@@ -25,10 +14,10 @@ import {
 import { useLocation, useHistory } from "react-router-dom";
 import { User } from "../Models";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import Carousel from "react-material-ui-carousel";
 import { shadows } from "@material-ui/system";
 import { Progress } from "react-sweet-progress";
 import "react-sweet-progress/lib/style.css";
+import Slideshow from "../Components/Slideshow";
 
 const useStyles = makeStyles<Theme, any>((theme) => ({
   page: {
@@ -90,94 +79,6 @@ const useStyles = makeStyles<Theme, any>((theme) => ({
   },
 }));
 
-const defaultOptions = {
-  loop: true,
-  autoplay: true,
-  animationData: legoData.default,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
-};
-const defaultOptions2 = {
-  loop: false,
-  autoplay: true,
-  animationData: doneData.default,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
-};
-
-const twitterbirdA = {
-  loop: false,
-  autoplay: true,
-  animationData: twitterbird.default,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
-};
-
-const geolocalizationA = {
-  loop: true,
-  autoplay: true,
-  animationData: geolocalization.default,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
-};
-
-const friendsA = {
-  loop: false,
-  autoplay: true,
-  animationData: friends.default,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
-};
-
-const verifiedA = {
-  loop: true,
-  autoplay: true,
-  animationData: verified.default,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
-};
-
-const analyzeA = {
-  loop: true,
-  autoplay: true,
-  animationData: analyze.default,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
-};
-
-const pressA = {
-  loop: true,
-  autoplay: true,
-  animationData: press.default,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
-};
-
-const abrahamA = {
-  loop: true,
-  autoplay: true,
-  animationData: abraham.default,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
-};
-
-const developmentA = {
-  loop: true,
-  autoplay: true,
-  animationData: development.default,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
-};
 export interface LoadingProps {}
 
 const sleep = (milliseconds: number) => {
@@ -214,6 +115,7 @@ const Loading: FC<LoadingProps> = (props) => {
         }
       );
   };
+
 
   const gettwitterdata = (usr: string) => {
     fetch(`${url}/gettwitterdata?username=${usr}&count=${sliderValue}`)
@@ -267,125 +169,7 @@ const Loading: FC<LoadingProps> = (props) => {
     statusMethod();
   });
 
-  function Slideshow(props) {
-    var items = [
-      {
-        description:
-          "Your Tweets are getting analyzed and assigned a happiness score between 1 and 9.",
-        image: (
-          <Lottie
-            options={analyzeA}
-            height={120}
-            width={120}
-            isClickToPauseDisabled
-          />
-        ),
-      },
-      {
-        description:
-          "You can press on the left or right side of the screen to move forwards and backwards.",
-        image: (
-          <Lottie
-            options={pressA}
-            height={120}
-            width={120}
-            isClickToPauseDisabled
-          />
-        ),
-      },
-      {
-        description:
-          '"Folks are usually about as happy as they make their minds up to be" \n - Abraham Lincoln.',
-        image: (
-          <Lottie
-            options={abrahamA}
-            height={120}
-            width={120}
-            isClickToPauseDisabled
-          />
-        ),
-      },
-      {
-        description:
-          "Wow! You have posted " +
-          userinfo.statuses_count +
-          " Tweets since you created your account. Are you OK?",
-        image: (
-          <Lottie
-            options={twitterbirdA}
-            height={120}
-            width={120}
-            isClickToPauseDisabled
-          />
-        ),
-      },
-      {
-        description:
-          "You have " +
-          userinfo.friends_count +
-          " friends on Twitter! Hopefully you have some in real life too.",
-        image: (
-          <Lottie
-            options={friendsA}
-            height={120}
-            width={120}
-            isClickToPauseDisabled
-          />
-        ),
-      },
-      {
-        description: userinfo.verified
-          ? "Your account is verified! I guess you're a pretty big thing, huh?"
-          : "Looks like your account isn't verified. Are you not famous enough?",
-        image: (
-          <Lottie
-            options={verifiedA}
-            height={120}
-            width={120}
-            isClickToPauseDisabled
-          />
-        ),
-      },
-    ];
 
-    return (
-      <Carousel
-        navButtonsAlwaysInvisible={true}
-        indicators={false}
-        animation="fade"
-        interval={8000}
-      >
-        {items.map((item, i) => (
-          <Item key={i} item={item} />
-        ))}
-      </Carousel>
-    );
-  }
-
-  function Item(props) {
-    return (
-      <Paper className={classes.paper} elevation={3}>
-        <Grid
-          container
-          spacing={5}
-          direction="column"
-          alignItems="center"
-          justify="center"
-        >
-          <Grid item xs={12} sm container>
-            <Grid item xs container spacing={2}>
-              <Grid item xs>
-                <Typography variant="body1" gutterBottom>
-                  {props.item.description}
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid item>{props.item.image}</Grid>
-          </Grid>
-        </Grid>
-      </Paper>
-    );
-  }
 
   return (
     <div className={classes.page}>
@@ -404,7 +188,7 @@ const Loading: FC<LoadingProps> = (props) => {
           )}
         </div>
         <div className={classes.slideshow}>
-          <Slideshow />
+          <Slideshow userinfo={userinfo} />
         </div>
 
         <Progress
@@ -413,6 +197,7 @@ const Loading: FC<LoadingProps> = (props) => {
           status={status}
           symbolClassName={classes.symbol}
         />
+
       </div>
 
       <Typography variant="h6">Choose a preloaded user instead</Typography>
