5f82c7eaf1b4a2f5b78cf6b41d45c308b9604371
Toheed Sharif <info@toheed.dk>
2021-04-26 09:48:55 +0200

    with timeout

--- my-app/src/Screens/Loading.tsx
+++ my-app/src/Screens/Loading.tsx
@@ -20,14 +20,13 @@ import {
   Box,
   Button,
   ButtonBase,
-  Avatar
+  Avatar,
 } from "@material-ui/core";
 import { useLocation, useHistory } from "react-router-dom";
 import { User } from "../Models";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Carousel from "react-material-ui-carousel";
-import { shadows } from '@material-ui/system';
-
+import { shadows } from "@material-ui/system";
 
 const useStyles = makeStyles<Theme, any>((theme) => ({
   page: {
@@ -38,15 +37,14 @@ const useStyles = makeStyles<Theme, any>((theme) => ({
     padding: "30px",
     margin: "auto",
     width: "100%",
-    height: "100vh"
+    height: "100vh",
   },
   center: {
     margin: "auto",
   },
-  slideshow: {
-  },
+  slideshow: {},
   header: {
-    marginBottom: "30px"
+    marginBottom: "30px",
   },
   paper: {
     display: "flex",
@@ -57,29 +55,28 @@ const useStyles = makeStyles<Theme, any>((theme) => ({
     maxWidth: 500,
     height: 184,
     borderRadius: "20px",
-    boxShadow: "white"
-
+    boxShadow: "white",
   },
   avatar: {
     height: 50,
     width: 50,
-    boxShadow: "0 0 0 1px #000"
+    boxShadow: "0 0 0 1px #000",
   },
   avatarbox: {
     display: "flex",
     flexDirection: "row",
-    justifyContent: "center",
+    justifyContent: "space-between",
     alignItems: "center",
     background: "white",
     borderRadius: "20px",
-    padding: "10px 0px 10px 0px",
+    padding: "10px 10px 10px 10px",
     width: "70%",
-    margin: "14px"
+    margin: "10px",
   },
   avatartext: {
     color: "black",
-    padding: "10px"
-  }
+    padding: "10px",
+  },
 }));
 
 const defaultOptions = {
@@ -87,16 +84,16 @@ const defaultOptions = {
   autoplay: true,
   animationData: legoData.default,
   rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice"
-  }
+    preserveAspectRatio: "xMidYMid slice",
+  },
 };
 const defaultOptions2 = {
   loop: false,
   autoplay: true,
   animationData: doneData.default,
   rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice"
-  }
+    preserveAspectRatio: "xMidYMid slice",
+  },
 };
 
 const twitterbirdA = {
@@ -104,8 +101,8 @@ const twitterbirdA = {
   autoplay: true,
   animationData: twitterbird.default,
   rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice"
-  }
+    preserveAspectRatio: "xMidYMid slice",
+  },
 };
 
 const geolocalizationA = {
@@ -113,8 +110,8 @@ const geolocalizationA = {
   autoplay: true,
   animationData: geolocalization.default,
   rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice"
-  }
+    preserveAspectRatio: "xMidYMid slice",
+  },
 };
 
 const friendsA = {
@@ -122,8 +119,8 @@ const friendsA = {
   autoplay: true,
   animationData: friends.default,
   rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice"
-  }
+    preserveAspectRatio: "xMidYMid slice",
+  },
 };
 
 const verifiedA = {
@@ -131,8 +128,8 @@ const verifiedA = {
   autoplay: true,
   animationData: verified.default,
   rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice"
-  }
+    preserveAspectRatio: "xMidYMid slice",
+  },
 };
 
 const analyzeA = {
@@ -140,8 +137,8 @@ const analyzeA = {
   autoplay: true,
   animationData: analyze.default,
   rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice"
-  }
+    preserveAspectRatio: "xMidYMid slice",
+  },
 };
 
 const pressA = {
@@ -149,8 +146,8 @@ const pressA = {
   autoplay: true,
   animationData: press.default,
   rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice"
-  }
+    preserveAspectRatio: "xMidYMid slice",
+  },
 };
 
 const abrahamA = {
@@ -158,8 +155,8 @@ const abrahamA = {
   autoplay: true,
   animationData: abraham.default,
   rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice"
-  }
+    preserveAspectRatio: "xMidYMid slice",
+  },
 };
 
 const developmentA = {
@@ -167,71 +164,50 @@ const developmentA = {
   autoplay: true,
   animationData: development.default,
   rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice"
-  }
+    preserveAspectRatio: "xMidYMid slice",
+  },
 };
 export interface LoadingProps {}
 
-const sleep = (milliseconds : number) => {
-  return new Promise(resolve => setTimeout(resolve, milliseconds))
-}
+const sleep = (milliseconds: number) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
 
 const Loading: FC<LoadingProps> = (props) => {
   const classes = useStyles({});
   const history = useHistory();
   const location = useLocation();
   const userinfo: User = location.state.memberDetail;
-  const sliderValue : Number = location.state.slider;
+  const sliderValue: Number = location.state.slider;
   const [done, setDone] = React.useState(false);
-  const [loading, setLoading] = React.useState(true)
-  const [loading2, setLoading2] = React.useState(true)
+  const [loading, setLoading] = React.useState(true);
+  const [loading2, setLoading2] = React.useState(true);
 
-  const url = "https://sharifhome.duckdns.org"
+  const url = "https://sharifhome.duckdns.org";
   //const url = "http://127.0.0.1:5000"
 
   const checkUsername = (usr: string) => {
-    fetch(`${url}/checkusername?username=${usr}`)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          (result["Userdata"]) ? getUserinfo(usr) : checkUsername(usr)
-        },
-        (error) => {
-          console.log(error)
-        }
-      )
-  }
+    setTimeout(() => {
+      fetch(`${url}/checkusername?username=${usr}`)
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            result["Userdata"] ? getUserinfo(usr) : checkUsername(usr);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    }, 3000);
+  };
 
   const gettwitterdata = (usr: string) => {
     fetch(`${url}/gettwitterdata?username=${usr}&count=${sliderValue}`)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log(result)
-          checkUsername(usr)
-        },
-        (error) => {
-          console.log(error)
-        }
-      )
-  }
-
-  const getUserinfo = (usr: string) => {
-    fetch(
-      `${url}/getdata?username=${usr}`
-    )
       .then((res) => res.json())
       .then(
         (result) => {
-          if (result["Error"]) checkUsername(usr)
-          else{
           console.log(result);
-
-          history.push({
-            pathname: "/story",
-            state: { memberDetail: result },
-          });
-        }
+          checkUsername(usr);
         },
         (error) => {
           console.log(error);
@@ -239,26 +215,46 @@ const Loading: FC<LoadingProps> = (props) => {
       );
   };
 
-  const loadBarackObama = () => {
-    fetch('preloaded/BarackObama.json'
-    ,{
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
+  const getUserinfo = (usr: string) => {
+    fetch(`${url}/getdata?username=${usr}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          if (result["Error"]) checkUsername(usr);
+          else {
+            console.log(result);
+
+            history.push({
+              pathname: "/story",
+              state: { memberDetail: result },
+            });
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
+
+  const loadpreloaded = (usr: string) => {
+    fetch(`preloaded/${usr}.json`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
     })
       .then((res) => res.json())
       .then(
         (result) => {
-          if (result["Error"]) checkUsername(usr)
-          else{
-          console.log(result);
+          if (result["Error"]) checkUsername(usr);
+          else {
+            console.log(result);
 
-          history.push({
-            pathname: "/story",
-            state: { memberDetail: result },
-          });
-        }
+            history.push({
+              pathname: "/story",
+              state: { memberDetail: result },
+            });
+          }
         },
         (error) => {
           console.log(error);
@@ -271,40 +267,84 @@ const Loading: FC<LoadingProps> = (props) => {
     gettwitterdata(userinfo.username);
   });
 
-
   function Slideshow(props) {
     var items = [
       {
-        description: "Your Tweets are getting analyzed and assigned a happiness score between 1 and 9.",
-        image: <Lottie options={analyzeA} height={120} width={120} isClickToPauseDisabled />
+        description:
+          "Your Tweets are getting analyzed and assigned a happiness score between 1 and 9.",
+        image: (
+          <Lottie
+            options={analyzeA}
+            height={120}
+            width={120}
+            isClickToPauseDisabled
+          />
+        ),
       },
       {
-        description: "You can press on the left or right side of the screen to move forwards and backwards.",
-        image: <Lottie options={pressA} height={120} width={120} isClickToPauseDisabled />
+        description:
+          "You can press on the left or right side of the screen to move forwards and backwards.",
+        image: (
+          <Lottie
+            options={pressA}
+            height={120}
+            width={120}
+            isClickToPauseDisabled
+          />
+        ),
       },
       {
-        description: "\"Folks are usually about as happy as they make their minds up to be\" \n - Abraham Lincoln.",
-        image: <Lottie options={abrahamA} height={120} width={120} isClickToPauseDisabled />
+        description:
+          '"Folks are usually about as happy as they make their minds up to be" \n - Abraham Lincoln.',
+        image: (
+          <Lottie
+            options={abrahamA}
+            height={120}
+            width={120}
+            isClickToPauseDisabled
+          />
+        ),
       },
       {
         description:
           "Wow! You have posted " +
           userinfo.statuses_count +
           " Tweets since you created your account. Are you OK?",
-        image: <Lottie options={twitterbirdA} height={120} width={120} isClickToPauseDisabled />,
+        image: (
+          <Lottie
+            options={twitterbirdA}
+            height={120}
+            width={120}
+            isClickToPauseDisabled
+          />
+        ),
       },
       {
         description:
           "You have " +
           userinfo.friends_count +
           " friends on Twitter! Hopefully you have some in real life too.",
-        image: <Lottie options={friendsA} height={120} width={120} isClickToPauseDisabled />
+        image: (
+          <Lottie
+            options={friendsA}
+            height={120}
+            width={120}
+            isClickToPauseDisabled
+          />
+        ),
       },
       {
         description: userinfo.verified
           ? "Your account is verified! I guess you're a pretty big thing, huh?"
           : "Looks like your account isn't verified. Are you not famous enough?",
-        image: <Lottie options={verifiedA} height={120} width={120} isClickToPauseDisabled />
+        image: (
+          <Lottie
+            options={verifiedA}
+            height={120}
+            width={120}
+            isClickToPauseDisabled
+          />
+        ),
       },
     ];
 
@@ -324,64 +364,103 @@ const Loading: FC<LoadingProps> = (props) => {
 
   function Item(props) {
     return (
-        <Paper className={classes.paper} elevation={3}>
-          <Grid container spacing={5} direction="column" alignItems="center" justify="center">
-            <Grid item xs={12} sm container>
-              <Grid item xs container spacing={2}>
-                <Grid item xs>
-                  <Typography variant="body1" gutterBottom>
-                    {props.item.description}
-                  </Typography>
-                </Grid>
-              </Grid>
-              <Grid item>
-               {props.item.image}
+      <Paper className={classes.paper} elevation={3}>
+        <Grid
+          container
+          spacing={5}
+          direction="column"
+          alignItems="center"
+          justify="center"
+        >
+          <Grid item xs={12} sm container>
+            <Grid item xs container spacing={2}>
+              <Grid item xs>
+                <Typography variant="body1" gutterBottom>
+                  {props.item.description}
+                </Typography>
               </Grid>
             </Grid>
+            <Grid item>{props.item.image}</Grid>
           </Grid>
-        </Paper>
+        </Grid>
+      </Paper>
     );
   }
 
   return (
     <div className={classes.page}>
-      <div className={classes.center} >
-      <div className={classes.header}>
-      {!done ? (
-          <FadeIn>
-            <div className="d-flex justify-content-center align-items-center">
-            <Typography align="center" variant="h5" component="h5">
-            Fetching Twitter data
-            </Typography>
-            </div>
-          </FadeIn>
-          
+      <div className={classes.center}>
+        <div className={classes.header}>
+          {!done ? (
+            <FadeIn>
+              <div className="d-flex justify-content-center align-items-center">
+                <Typography align="center" variant="h5" component="h5">
+                  Fetching Twitter data
+                </Typography>
+              </div>
+            </FadeIn>
+          ) : (
+            <h1>Done</h1>
+          )}
+        </div>
+        <div className={classes.slideshow}>
+          <Slideshow />
+        </div>
+        {loading ? (
+          <Lottie options={defaultOptions} height={120} width={120} />
         ) : (
-          <h1>Done</h1>
+          <Lottie options={defaultOptions2} height={120} width={120} />
         )}
-      </div>
-      <div className={classes.slideshow}>
-        <Slideshow />
-      </div>
-      {loading ? (
-                <Lottie options={defaultOptions} height={120} width={120} />
-              ) : (
-                <Lottie options={defaultOptions2} height={120} width={120} />
-              )}
       </div>
 
       <Typography variant="h6">Choose a preloaded user instead</Typography>
-      <Box onClick={() => loadBarackObama()} className={classes.avatarbox} boxShadow={3}>
-          <Avatar  alt="Remy Sharp" src={"https://pbs.twimg.com/profile_images/1329647526807543809/2SGvnHYV.jpg"} className={classes.avatar} />
-          <Typography variant="subtitle1" className={classes.avatartext}>Barack Obama</Typography>
+      <Box
+        onClick={() => loadpreloaded("TimCook")}
+        className={classes.avatarbox}
+        boxShadow={3}
+      >
+        <Avatar
+          alt="Tim Cook"
+          src={
+            "https://pbs.twimg.com/profile_images/1194113737092935681/63O1znGw.jpg"
+          }
+          className={classes.avatar}
+        />
+        <Typography variant="subtitle1" className={classes.avatartext}>
+          Tim Cook
+        </Typography>
       </Box>
-      <Box onClick={() => console.log("clicked")} className={classes.avatarbox} boxShadow={3}>
-          <Avatar  alt="Remy Sharp" src={"https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg"} className={classes.avatar} />
-          <Typography variant="subtitle1" className={classes.avatartext}>Remy Sharp</Typography>
+      <Box
+        onClick={() => loadpreloaded("BillGates")}
+        className={classes.avatarbox}
+        boxShadow={3}
+      >
+        <Avatar
+          alt="Bill Gates"
+          src={
+            "https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X.jpg"
+          }
+          className={classes.avatar}
+        />
+        <Typography variant="subtitle1" className={classes.avatartext}>
+          Bill Gates
+        </Typography>
       </Box>
-      <Box onClick={() => console.log("clicked")} className={classes.avatarbox} boxShadow={3}>
-          <Avatar  alt="Remy Sharp" src={"https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg"} className={classes.avatar} />
-          <Typography variant="subtitle1" className={classes.avatartext}>Remy Sharp</Typography>
+      <Box
+        onClick={() => loadpreloaded("Potus")}
+        className={classes.avatarbox}
+        boxShadow={3}
+      >
+        <Avatar
+          alt="President Biden"
+          src={
+            "https://pbs.twimg.com/profile_images/1380530524779859970/TfwVAbyX.jpg"
+          }
+          className={classes.avatar}
+        />
+        <Typography variant="subtitle1" className={classes.avatartext}>
+          President Biden
+        </Typography>
       </Box>
     </div>
   );
