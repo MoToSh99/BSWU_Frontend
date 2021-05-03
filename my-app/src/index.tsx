22baddc386f5a6502be879f8c9175290ba420e1f
Toheed Sharif <info@toheed.dk>
2021-05-03 13:38:32 +0200

    store added again

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { StoreProvider } from 'easy-peasy';
+import { store } from './store';
+
 
 ReactDOM.render(
+    <StoreProvider store={store}> 
         <App />
+    </StoreProvider>
     , document.getElementById('root'));
 
 serviceWorker.unregister();
